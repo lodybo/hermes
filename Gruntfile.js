76984e0afdece596c80440953ea0f86710d1cfcb
Lody Borgers <lody.borgers@capgemini.com>
2016-07-27 16:35:10 +0200

    Set up project

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      jshintrc: ".jshintrc",
+      files: ['Gruntfile.js', 'src/js/*.js', 'test/specs/*.js'],
+    },
+    jasmine: {
+      all: {
+        options: {
+          specs: "test/specs/*.spec.js",
+          outFile: "test/jasmine/_SpecRunner.html",
+          template: require('grunt-template-jasmine-istanbul'),
+          templateOptions: {
+              coverage: 'test/jasmine/coverage/coverage.json',
+              report: 'test/jasmine/coverage',
+              thresholds: {
+                  lines: 75,
+                  statements: 75,
+                  branches: 75,
+                  functions: 90
+              }
+          }
+        },
+        src: "src/js/*.js"
+      }
+    },
+    jscs: {
+      options: {
+        config: ".jscsrc"
+      },
+      files: {
+        src: ["src/js/*.js"]
+      }
+    },
+    uglify: {
+      minified: {
+        options: {
+          sourceMaps: true,
+          sourceMapName: "dist/hermes.min.js.map"
+        },
+        files: {
+          "dist/hermes.min.js" : ["src/js/*.js"]
+        }
+      },
+      compressed: {
+        options: {
+          mangle: false,
+        },
+        files: {
+          "dist/hermes.comp.js" : ["src/js/*.js"]
+        }
+      },
+      beautified: {
+        options: {
+          beautify: true
+        },
+        files: {
+          "dist/hermes.js" : ["src/js/*.js"]
+        }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['jshint']);
+
+};
